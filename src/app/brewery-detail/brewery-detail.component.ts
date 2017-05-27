43fa2c88cd7925cc864e5eafdf66ef2cb734512a
m-ciesielski <mateusz.jan.ciesielski@gmail.com>
2017-05-27 16:11:26 +0200

    Show beers in brewery details

--- src/app/brewery-detail/brewery-detail.component.ts
+++ src/app/brewery-detail/brewery-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 
@@ -18,14 +18,26 @@ export class BreweryDetailComponent implements OnInit {
   @Input() brewery: Brewery;
   beers: Beer[];
   constructor(private breweryService: BreweryService, private beerService: BeerService,
-              private route: ActivatedRoute, private location: Location) { }
+              private route: ActivatedRoute, private location: Location, private router: Router) { }
   ngOnInit() {
     this.route.params.
     switchMap((params: Params) => this.breweryService.getBrewery(+params['id']))
-      .subscribe(brewery => this.brewery = brewery);
-    // TODO: FIX this
+      .subscribe(brewery => {
+          this.brewery = brewery;
+          this.getBeers();
+        }
+      );
+  }
+
+  getBeers(): void {
     this.beerService.getBeers().then(
       beers => this.beers = beers.filter(b => b.brewery !== undefined).filter(b => b.brewery.id === this.brewery.id));
+  }
+  onSelect(beer: Beer): void {
+    this.gotoBeer(beer);
+  }
+  gotoBeer(beer: Beer): void {
+    this.router.navigate(['/beers', beer.id]);
   }
   // TODO: Add canDeactivate guard, so user cannot exit app using goBack method
   goBack(): void {
